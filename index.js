7e8d503179b6a17fab6c45e6ce69963e4d5c8f3b
SmitiShakya <smitishk12@gmail.com>
2019-08-22 14:54:48 -0500

    finished1nd2step

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 'use strict';
-let numOfDog;
-
-function displayResults(responseJson) {
-  console.log(responseJson.message);
-}
 
 
-function getDogImage() {
-  fetch(`https://dog.ceo/api/breeds/image/random/${numOfDog}`)
-    .then(response => response.json())
-    .then(responseJson => 
+function getDogImage(inputNo){
+  fetch(`https://dog.ceo/api/breeds/image/random/${inputNo}`)
+  .then(response => response.json())
+   .then(responseJson =>
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  let imgAry = responseJson.message;
+  imgAry.forEach(img => {
+    $('.show').append(
+      `<img src="${img}" class="show">`
+    );
+  });
+}
+
 
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    numOfDog = $('#number-input').val();
-    getDogImage();
+    let inputNo = $('#numDogs').val();
+    getDogImage(inputNo);
   });
 }
 
-$(watchForm);
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
